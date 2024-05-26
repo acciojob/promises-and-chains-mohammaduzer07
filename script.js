3fd3804f9a18f8e720525cbf0d9d3539a0ec3abf
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-05-26 16:21:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+let age = documet.getElementById('age');
+let name = documet.getElementById('name');
+let btn = documet.getElementById('btn');
+
+btn.addEventListener('click', display());
+
+
+function display(age) {
+	return new Promise((resolve) =>{
+		setTimeout(() =>{
+			resolve(age);
+		}, 4000);
+	});
+}
+
+display(age)
+.then((res) =>{
+	const age1 = age.innerText;
+	if(age1 > 18){
+		return `welcome, ${name}. You can vote`
+	} else {
+		re
+	}
+})
